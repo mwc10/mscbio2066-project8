02abc6f985a79df16047222e6bb145e2abc6ab07
mwc <43445389+mwc10@users.noreply.github.com>
2023-04-22 21:36:58 -0400

    add configs without compound filter

---
+++ make_configs.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import json
+
+RADII = [1, 2, 3, 5, 7, 9, 11]
+BITS = [1024, 2048]
+ACTIVITIES = [['KD', 'KI'], ['KD', 'KI', 'EC50'], ['KD', 'KI', 'EC50', 'IC50']]
+
+def make_fp(radius, bits):
+    return {
+        'file': f'ecfp-r{radius}-b{bits}.pkl',
+        'bitSize': bits,
+        'radius': radius,
+        'useFeatures': False,
+        'useChirality': False,
+    }
+
+def make_config(radius, bits, acts):
+    return {
+        'kinases': {
+            'file': 'map-uniprot-seq.tsv',
+            'species': ['Human'],
+        },
+        'fingerprints': make_fp(radius, bits),
+        'dataFile': 'full-median.parquet',
+        'activityTypes': acts,
+        'optRounds': 150,
+    }
+
+def make_fname(radius, bits, acts):
+    return f'hu-fp{bits}r{radius}-{"".join(acts)}.json'
+
+def pair(*args):
+    return make_fname(*args), make_config(*args)
+
+sets = [pair(r,b,a) for r in RADII for b in BITS for a in ACTIVITIES]
+
+for f, c in sets:
+    json.dump(c, open(f'configs/{f}', 'w'), indent=4)
