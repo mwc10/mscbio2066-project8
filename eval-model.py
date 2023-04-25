f77317d658ad12138e419336fc1671e1fc1ccf70
mwc <43445389+mwc10@users.noreply.github.com>
2023-04-25 12:09:45 -0400

    add validation testing to find best model

---
+++ eval-model.py
@@ -0,0 +1,83 @@
+from model import Model
+
+import polars as pl
+import numpy as np
+
+import argparse, multiprocessing as mp
+from itertools import cycle
+from pathlib import Path
+
+def cli():
+    p = argparse.ArgumentParser()
+    p.add_argument('--models', '-m', required=True, type=Path, help='Directory containing model tar.gz bundles')
+    p.add_argument('--scores', '-s', required=True, type=Path, help='Parquet file with scored sequences')
+    p.add_argument('--evals', '-e', required=True, type=Path, help='Directory contain evaluation text files (space separated)')
+    p.add_argument('--output', '-o', default=None, type=Path, help='Output for CSV, stdout otherwise')
+
+    return p.parse_args()
+
+def record_model_info(m: Model, corrcoef: np.ndarray):
+    boosters, inferred = m.describe()
+    c = m.config
+    fp = c['fingerprints']
+    cf = corrcoef[0, 1]
+    mt = c['metrics']
+
+    return {
+        'bit_size': fp['bitSize'],
+        'use_features': fp['useFeatures'],
+        'use_chirality': fp['useChirality'],
+        'radius': fp['radius'],
+        'activities': ', '.join(c['activityTypes']),
+        'metric': mt['metric'] if mt else None,
+        'metric_op': mt['op'] if mt else None,
+        'metric_val': mt['val'] if mt else None,
+        'min_cmpds': mt['min_cmpds'] if mt else 10,
+        'k': m.topK,
+        'boosters': boosters,
+        'inferred': inferred,
+        'corrcoef': cf,
+    }
+
+def run_prediction(args):
+    (mtar, r2min, cmin, k), scores, evals = args
+    m = Model(mtar, cores=1, scores=scores, metric_val=r2min, min_cmpds=cmin, verbose=False, similar_k=k)
+    predicted = m.predict_batch(evals['SMILES'], evals['UniProt'])
+    coef = np.corrcoef(evals['pKd'], predicted)
+    print(mtar, k, r2min, cmin, coef[0, 1], sep='\t')
+
+    return record_model_info(m, coef)
+
+# Constants
+R2_MIN = [None, -2.0, -1.0, -0.75, -0.5, -0.25, 0.0, 0.25, 0.5]
+CMPD_MIN = [10, 15, 30, 60, 120, 240]
+TOPK = [2, 3, 4, 5, 10]
+
+# main
+def main(args):
+    dfScores = pl.read_parquet(args.scores)
+    scores = {uid: nn for uid, _, nn in zip(*tuple(dfScores.to_dict().values()))}
+    evals = pl.concat(pl.scan_csv(p, separator=' ') for p in args.evals.glob('*.txt')).collect()
+
+    # for now
+    # mtar = next(args.models.glob('*.tar.gz'))
+    # print(mtar)
+    vrs = ((mtar, r2min, cmin, k) for k in TOPK for cmin in CMPD_MIN for r2min in R2_MIN for mtar in args.models.glob('*.tar.gz'))
+    itr = zip(vrs, cycle([scores]), cycle([evals]))
+    with mp.Pool() as p:
+        print('in pool')
+        output = list(p.imap_unordered(run_prediction, itr, chunksize=10))
+    
+    print('out of pool')
+    # output = [run_prediction(mtar, r2min, cmin) for cmin in CMPD_MIN for r2min in R2_MIN]
+    df = pl.from_dicts(output)
+    if args.output:
+        df.write_csv(args.output)
+    else:
+        print(df.write_csv())
+
+    return df
+
+if __name__ == '__main__':
+    args = cli()
+    df = main(args)
--- model.py
+++ model.py
@@ -11,7 +11,7 @@ from Bio import Align
 import requests
 
 import json
-from typing import IO, Dict, List, Sequence, Union
+from typing import IO, Dict, List, Optional, Sequence, Tuple, Union
 import heapq
 import tarfile
 import multiprocessing
@@ -20,7 +20,13 @@ class Model:
     def __init__(self,
         tar: Union[str, Path, IO[bytes]],
         cores: int=1,
-        similar_k: int=3
+        similar_k: int=3,
+        scores: Optional[Dict[str, List[str]]]=None,
+        metric: str='r2',
+        metric_op: str='min',
+        metric_val: Optional[float]=None,
+        min_cmpds: Optional[int]=None,
+        verbose=True,
     ) -> None:
         def load_model(b: bytearray):
             r = xgb.XGBRegressor(random_state=42)
@@ -29,7 +35,7 @@ class Model:
             return r
         
         # Check tar to see if path, otherwise fileobject
-        if type(tar) is str or type(tar) is Path:
+        if type(tar) is str or isinstance(tar, Path):
             openParams = {'name': tar, 'mode':'r'}
         else:
             openParams = {'fileobj': tar, 'mode':'r'}
@@ -42,14 +48,48 @@ class Model:
             ubj_bytes = lambda p: bytearray(tf.extractfile(p).read())
             configure = lambda tup: (tup[0], {'models': [load_model(ubj_bytes(tup[1]))], 'seq': tup[2]})
             df = pd.read_csv(tf.extractfile('info.csv'))
+
+            # filter trees by training metrics or by number of compounds
+            if min_cmpds is not None or metric_val is not None:
+                match (metric_op, metric_val):
+                    case (_, None): qm = None
+                    case ('min', _): qm = f'{metric} >= {metric_val}'
+                    case ('max', _): qm = f'{metric} <= {metric_val}'
+                
+                qc = f'num_compounds >= {min_cmpds}' if min_cmpds is not None else None
+                q = ' and '.join(filter(lambda x: x is not None, [qm, qc]))
+                df = df.query(q)
+            
             itr = zip(df['uniprot'], df['output_model'], df['sequence'])
 
             self.protModels = dict(map(configure, itr))
             self.config = json.load(tf.extractfile('config.json'))
         
+        # store basic configuration parameters
+        self.verbose = verbose
         self.cores = cores
         self.topK = similar_k
+
+        # use the pre-configured blastp aligning scoring
+        # note that this has to match for pre-calculated scores
         self.aligner = Align.PairwiseAligner(scoring='blastp')
+        
+        # preprocess scored sequences for models that either were not made or were filtered
+        if scores is not None:
+            self.use_precalced(scores)
+
+        # save metric info to the config
+        if min_cmpds is not None or metric_val is not None:
+            metrics = {
+                'min_cmpds': min_cmpds, 
+                'metric': metric,
+                'op': metric_op,
+                'val': metric_val,
+                }
+            self.config['metrics'] = metrics
+        else:
+            self.config['metrics'] = None
+
 
     def predict(self, smiles: Union[str, npt.NDArray], uniprot: str) -> float:
         if type(smiles) is str:
@@ -58,6 +98,7 @@ class Model:
             fp = smiles
 
         if uniprot not in self.protModels:
+            if self.verbose: print('novel kinase target; fetching info from uniprot')
             seqs = self.fetch_seqs([uniprot])
             for uid, seq in seqs.items():
                 models = self.get_similar(seq)
@@ -68,30 +109,29 @@ class Model:
     def predict_batch(self, smiles: Sequence[str], uniprot: Sequence[str]) -> List[float]:
         unique_smiles = set(smiles)
         novel_uniprot = set(uniprot).difference(set(self.protModels.keys()))
-        print('total unique smiles', len(unique_smiles), '/', len(smiles))
-        print('total novel uniprot', len(novel_uniprot), '/', len(uniprot))
+        if self.verbose:
+            print('total unique smiles', len(unique_smiles), '/', len(smiles))
+            print('total novel uniprot', len(novel_uniprot), '/', len(set(uniprot)), '/', len(uniprot))
 
-        # Ideally this would all be done with multiprocessing...
-        novel_seqs = self.fetch_seqs(novel_uniprot)
-        print('got novel sequences from uniprot API')
-        # with multiprocessing.Pool(self.cores) as p:
-        #     models = p.map(self.get_similar, novel_seqs.values())
-        #     for (uid, seq), m in zip(novel_seqs.items(), models):
-        #         self.protModels[uid] = {'models': m, 'seq': seq}
+        if len(novel_uniprot) > 0:
+            novel_seqs = self.fetch_seqs(novel_uniprot)
+            if self.verbose: print('got novel sequences from uniprot API')
+            # Ideally this would all be done with multiprocessing...
+            # but it doesn't seem to work on the grader...
+            # with multiprocessing.Pool(self.cores) as p:
+            #     models = p.map(self.get_similar, novel_seqs.values())
+            #     for (uid, seq), m in zip(novel_seqs.items(), models):
+            #         self.protModels[uid] = {'models': m, 'seq': seq}
 
-        for uid, seq in novel_seqs.items():
-            models = self.get_similar(seq)
-            self.protModels[uid] = {'models': models, 'seq': seq}
-        print('found similar models for unique sequences')
+            for uid, seq in novel_seqs.items():
+                models = self.get_similar(seq)
+                self.protModels[uid] = {'models': models, 'seq': seq}
+            if self.verbose: print('found similar models for unique sequences')
 
-
-        fingerprints = {smi: self.fingerprint(smi).reshape(-1, 1) for smi in unique_smiles}
-        print('done fingerprinting')
-        return [self.predict(smi, uid) for smi, uid in zip(smiles, uniprot)]
-        print(fingerprints)
-        # for smiles, uid in zip(smiles, uniprot):
-        #     np.mean([m.predict(fingerprints[smiles]) for m in self.protModels[uid]])
-        # print(seqs)
+        fps = {smi: self.fingerprint(smi).reshape(1, -1) for smi in unique_smiles}
+        fps = [fps[smi] for smi in smiles]
+        if self.verbose: print('done fingerprinting')
+        return [self.predict(fp, uid) for fp, uid in zip(fps, uniprot)]
     
     def fingerprint(self, smiles: str) -> npt.NDArray[np.uint8]:
         mol = AllChem.MolFromSmiles(smiles)
@@ -126,6 +166,40 @@ class Model:
 
         return [x[1] for x in heapq.nlargest(self.topK, itr, key=lambda x: x[0])]
 
+    def use_precalced(self, scores):
+        ''' For testing, use pre-calculated similarities of all test seqs 
+            against all trained seqs. 
+            The selection of most similar models has to be done dynamically,
+            as the current forests in the model changes based on metric filter criteria
+        '''
+        for uid, nns in scores.items():
+            # there is an XGBoost tree for this kinase
+            if uid in self.protModels: continue;
+            # otherwise, pick the topK models that are closest to this one
+            models = []
+            for nnid in nns:
+                if nnid in self.protModels and len(self.protModels[nnid]['models']) == 1:
+                    models.append(self.protModels[nnid]['models'][0])
+                if len(models) >= self.topK: break;
+
+            self.protModels[uid] = {'models': models, 'seq': None}
+    
+    def describe(self) -> Tuple[int, int]:
+        ''' return the number of kinase boosters and inferred kinases '''
+        boosters, inferred = 0, 0
+        for info in self.protModels.values():
+            if len(info['models']) > 1:
+                inferred += 1
+            else:
+                boosters += 1
+        
+        return boosters, inferred
+
+    def __str__(self):
+        return f'''\
+        '''
+
+
 if __name__ == '__main__':
     import argparse
     import fsspec
@@ -135,8 +209,12 @@ if __name__ == '__main__':
         p = argparse.ArgumentParser('Run model in `tarfile` on `datafile`')
         p.add_argument('input', help='input columnar data file with SMILES and UniProt IDs')
         p.add_argument('output', nargs='?', default=None, help="save DF to file, or print to stdout if not present")
-        p.add_argument('--tarfile', '-t', default='gs://mwc10-mscbio2066/model_hu-b2048-r2-kikd.tar.gz', help='tar containing model info')
+        p.add_argument('--tarfile', '-t', default='gs://mwc10-mscbio2066/model_hu-fp2048r3-KDKI.tar.gz', help='tar containing model info')
         p.add_argument('--eval', '-e', action='store_true')
+        p.add_argument('--cmin', '-c', type=int, default=16, help='min amount of compounds used to train a kinase GBF')
+        p.add_argument('--rmin', '-r', type=float, default=None)
+        p.add_argument('-k', type=int, default=3, help='Infer from K models')
+        p.add_argument('--scores', '-s', default=None, help='precalculated kinase similiarities')
         return p.parse_args()
     
     def get_num_cores():
@@ -147,10 +225,20 @@ if __name__ == '__main__':
     
     args = cli()
     cores = get_num_cores()
+
     df = pd.read_csv(args.input, sep=' ').dropna(axis='columns', how='all')
+
+    if args.scores:
+        # too lazy to rewrite this to use pandas..
+        import polars as pl
+        dfScores = pl.read_parquet(args.scores)
+        scores = {uid: nn for uid, _, nn in zip(*tuple(dfScores.to_dict().values()))}
+        print('parsed pre-calculated scores')
+    else:
+        scores = None
     print(f'creating model for {args.tarfile}')
     with fsspec.open(args.tarfile) as tar:
-        m = Model(tar, cores)
+        m = Model(tar, cores, scores=scores, min_cmpds=args.cmin, metric_val=args.rmin, similar_k=args.k)
     predictions = m.predict_batch(df['SMILES'], df['UniProt'])
     df['Predicted'] = pd.Series(np.array(predictions), dtype=float)
     # test = df.with_columns([pl.Series('Predict pKd', predictions, dtype=pl.Float64)])
@@ -162,3 +250,6 @@ if __name__ == '__main__':
         print(df)
         corr = np.corrcoef(df['Predicted'], df['pKd']).min()
         print(f'Correlation with labels in input:\t{corr:.4f}')
+    
+    print(m.describe())
+    print(m.config)
